ddee001121214f2a05ceb3b6389471412a2b49ff
chikaakta <59062753+chikaakta@users.noreply.github.com>
2021-12-07 09:53:23 +0100

    fix

--- calc.py
+++ calc.py
@@ -14,18 +14,18 @@ def result():
     var_1 = request.form.get("var_1", type=int)
     var_2 = request.form.get("var_2", type=int)
     operation = request.form.get("operation")
-    if(operation == 'Addition'):
-        result = var_1 + var_2
-    elif(operation == 'Subtraction'):
-        result = var_1 - var_2
-    elif(operation == 'Multiplication'):
-        result = var_1 * var_2
-    elif(operation == 'Division'):
-        result = var_1 / var_2
+    if operation == 'Addition':
+        resultat = var_1 + var_2
+    elif operation == 'Subtraction':
+        resultat = var_1 - var_2
+    elif operation == 'Multiplication':
+        resultat = var_1 * var_2
+    elif operation == 'Division':
+        resultat = var_1 / var_2
     else:
-        result = 'INVALID CHOICE'
-    entry = result
-    return render_template('result.html', entry=entry)
+        resultat = 'INVALID CHOICE'
+
+    return render_template('result.html', entry=resultat)
 
 if __name__ == '__main__':
     app.run(debug=True)
